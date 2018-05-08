a9da26772af0c94026ddb89db21bebc861213be4
Kai Chen <kchen9530@gmail.com>
2018-05-08 17:25:45 -0400

    modify test.sh

--- awsTest/test.sh
+++ awsTest/test.sh
@@ -1,2 +1,2 @@
 echo 'testing apriori'
-spark-submit /home/ec2-user/BigDataProject/BruteForce.py /home/ec2-user/BigDataProject/ha.csv
+spark-submit /home/ec2-user/BigDataProject/BruteForce.py /home/ec2-user/BigDataProject/awsTest/file1.csv
