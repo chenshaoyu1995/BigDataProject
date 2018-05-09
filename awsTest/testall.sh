7749ab4daf09a77b4bcb7fd1a9f8566e5bdd7937
Kai Chen <kchen9530@gmail.com>
2018-05-08 21:23:20 -0400

    fix

--- awsTest/testall.sh
+++ awsTest/testall.sh
@@ -1,8 +1,7 @@
-root='/home/ec2-user/BigDataProject'
-test="$root/awsTest/test.sh"
+test="./home/ec2-user/BigDataProject/awsTest/test.sh"
 file=$1
 slaves=$3
-./$test $file BruteForce $slaves
-./$test $file HCA $slaves
-./$test $file improvedHCA $slaves
+$test $file BruteForce $slaves
+$test $file HCA $slaves
+$test $file improvedHCA $slaves
 
