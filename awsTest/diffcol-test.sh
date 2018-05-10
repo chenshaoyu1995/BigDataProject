9e7d67f8208afb1362900ecf9f3de18276485d24
Kai Chen <kchen9530@gmail.com>
2018-05-09 20:10:42 -0400

    add partial result

--- awsTest/diffcol-test.sh
+++ awsTest/diffcol-test.sh
@@ -1,4 +1,4 @@
 ./testall.sh parking-100000r-4c 8
 ./testall.sh parking-100000r-6c 8
-./testall.sh parking-100000r-10c 8
-./testall.sh parking-100000r-12c 8
+# ./testall.sh parking-100000r-10c 8
+# ./testall.sh parking-100000r-12c 8
