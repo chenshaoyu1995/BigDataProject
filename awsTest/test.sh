02ce9ef43fe4870a1bbe5d1b53c763b1af96a392
Kai Chen <kchen9530@gmail.com>
2018-05-12 18:42:15 -0400

    modify test script

--- awsTest/test.sh
+++ awsTest/test.sh
@@ -14,14 +14,7 @@ echo $outputFile
 echo $algFile
 
 spark-submit \
---master=spark://ec2-204-236-192-147.compute-1.amazonaws.com:7077
---driver-memory 1G \
---executor-cores 4 \
---executor-memory 8G \
---num-executors $slaves \
---conf spark.default.parallelism=1000 \
---conf spark.storage.memoryFraction=0.5 \
---conf spark.shuffle.memoryFraction=0.3 \
+--master=spark://ec2-204-236-192-147.compute-1.amazonaws.com:7077 \
 $algFile $inputFile | python $filterFile > $outputFile
 
   
